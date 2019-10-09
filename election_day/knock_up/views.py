b09bc4bb842a41f832145ed8ba234a69b99a7252
Nigel Tart <nigeltart@gmail.com>
2019-10-09 10:25:29 +0100

    Had a go at paths for the views we created
    I copied the path in url.py for each view, but not sure whether I should use the view name three times, eg
       path('knocker_delete', views.knocker_delete, name='knocker_delete'),
    
    I also added a logon view for the empty path:
    def logon(request):
        return HttpResponse("log on as admin, teller or user")

--- election_day/knock_up/views.py
+++ election_day/knock_up/views.py
@@ -3,29 +3,31 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+def logon(request):
+    return HttpResponse("log on as admin, teller or user")
 
-def add_voter(request):
+def teller_add_voter(request):
     return HttpResponse("ask for voter number")
 
-def check_voter(request):
+def teller_check_voter(request):
     return HttpResponse("review ")
 
-def delete_voter(request):
+def teller_delete_voter(request):
     return HttpResponse("which one was entered wrongly")
 
-def list_voters(request):
+def admin_list_voters(request):
     """
     Admin View
     Promised votes who haven't yet actually voted
     """
     return HttpResponse("admin sees voters to assign to knockers")
 
-def assign_voters(request):
+def admin_assign_voters(request):
     return HttpResponse("admin assigns voters to knockers")
 
 def knocker_list (request):
     return HttpResponse("list of people to knock up")
 
 def knocker_delete (request):
-    return HttpResponse("don't knock these voters again")
+    return HttpResponse("don't knock up these voters again")
 
